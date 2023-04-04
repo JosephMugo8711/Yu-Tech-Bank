da17ca57da99f1e0dee8686b3e6153fbe19d9d85
josephwamiti <wamiti8711@gmail.com>
2023-04-04 04:14:03 +0300

    adding routes usercontroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,23 @@
 class UsersController < ApplicationController
 
-    
+    #Retrieve all users
+    def index
+        user = User.all
+        render json: user, status: :ok
+    end
+
+    def show
+        user = User.find_by(id: params[:id])
+        render json: user, status: :ok
+    end
+
+
+
+    private
+
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :email, :phone_number, :password)
+    end
 
 
 end
