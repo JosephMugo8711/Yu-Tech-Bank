c30e388af291d0c4ad793c425a0fd0e1cc0a8473
josephwamiti <wamiti8711@gmail.com>
2023-04-11 04:41:18 +0300

    components arrangement

---
+++ yutek-frontend/src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,10 @@
+//Build a user profile page that displays the user's information and 
+//allows them to update their profile picture, phone number, and address.
+
+import React from "react";
+
+const Dashboard = () => {
+    return(
+        
+    )
+}
